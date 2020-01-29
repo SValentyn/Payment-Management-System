1c2f8c2c2b7164b62a03bc3dc11cea006db54339
Syniuk Valentyn <planet.sv01@gmail.com>
2020-01-29 17:39:10 +0200

    Added request parameters (for more information) to CommandBlockAccount.java

--- src/main/java/com/system/command/CommandBlockAccount.java
+++ src/main/java/com/system/command/CommandBlockAccount.java
@@ -3,6 +3,8 @@ package com.system.command;
 import com.system.entity.User;
 import com.system.manager.ResourceManager;
 import com.system.service.AccountService;
+import com.system.service.CreditCardService;
+import com.system.service.PaymentService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,10 +20,14 @@ public class CommandBlockAccount implements ICommand {
 
         if (accountId != null) {
             AccountService.getInstance().blockAccount(Integer.parseInt(accountId));
+
+            request.setAttribute("showAccounts", true);
+            request.setAttribute("showAccountInfo", true);
+            request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
+            request.setAttribute("cards", CreditCardService.getInstance().findCardsByAccountId(Integer.parseInt(accountId)));
+            request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(Integer.parseInt(accountId)));
         }
 
-        request.setAttribute("showAccounts", true);
-        request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
         return ResourceManager.getInstance().getProperty(ResourceManager.HOME);
     }
 
