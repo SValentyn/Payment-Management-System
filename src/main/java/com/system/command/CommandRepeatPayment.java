a0099364bb58cb04dd37255f67e67835cee51bc5
Syniuk Valentyn <planet.sv01@gmail.com>
2020-01-30 08:55:56 +0200

    Refactoring the CommandRepeatPayment class

--- src/main/java/com/system/command/CommandRepeatPayment.java
+++ src/main/java/com/system/command/CommandRepeatPayment.java
@@ -2,11 +2,9 @@ package com.system.command;
 
 import com.system.entity.Payment;
 import com.system.entity.User;
-import com.system.manager.HTTPMethod;
 import com.system.manager.ResourceManager;
 import com.system.persistence.factory.DaoFactory;
 import com.system.service.AccountService;
-import com.system.service.PaymentService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,39 +13,23 @@ import java.sql.SQLException;
 
 public class CommandRepeatPayment implements ICommand {
 
-    private static final double PERCENT = 0.01;
-
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
         String paymentId = request.getParameter("paymentId");
         User user = (User) request.getSession().getAttribute("currentUser");
-        Payment payment = null;
-        Integer accountId = null;
-        String number = null;
-        BigDecimal sum = null;
-        String appointment = null;
 
-        if (paymentId != null) {
-            payment = DaoFactory.createPaymentDao().findPaymentByPaymentId(Integer.parseInt(paymentId));
-            accountId = payment.getAccountId();
-            number = payment.getCardNumber();
-            sum = payment.getSum();
-            appointment = payment.getAppointment();
-        }
+        Payment payment = DaoFactory.createPaymentDao().findPaymentByPaymentId(Integer.parseInt(paymentId));
+        String number = payment.getCardNumber();
+        BigDecimal amount = payment.getSum();
+        String appointment = payment.getAppointment();
 
-        String method = request.getMethod();
-        if (method.equalsIgnoreCase(HTTPMethod.GET.name())) {
-            request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
-            request.setAttribute("numberValue", number);
-            request.setAttribute("summaValue", sum);
-            request.setAttribute("appointmentValue", appointment);
-            return ResourceManager.getInstance().getProperty(ResourceManager.MAKE_PAYMENT);
-        } else if (method.equalsIgnoreCase(HTTPMethod.POST.name())) {
-            PaymentService.getInstance().formPayment(accountId, number, sum, appointment);
-        }
+        request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
+        request.setAttribute("numberValue", number);
+        request.setAttribute("amountValue", amount);
+        request.setAttribute("appointmentValue", appointment);
 
-        return ResourceManager.getInstance().getProperty(ResourceManager.HOME);
+        return ResourceManager.getInstance().getProperty(ResourceManager.MAKE_PAYMENT);
     }
 
 }
