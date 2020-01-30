e6477c340f9a4612ac29faaa613886c8c7b7e46e
Syniuk Valentyn <planet.sv01@gmail.com>
2020-01-31 00:36:10 +0200

    Fixed test TestPaymentService.java

--- src/test/java/com/system/service/TestPaymentService.java
+++ src/test/java/com/system/service/TestPaymentService.java
@@ -44,13 +44,13 @@ public class TestPaymentService {
         account_2.setBalance(new BigDecimal(50_000));
         account_2.setIsBlocked(false);
 
-        when(paymentService.formPayment(payment.getAccountId(), payment.getCardNumber(), new BigDecimal(1_000), new Double(1), payment.getAppointment()))
+        when(paymentService.formPayment(payment.getAccountId(), payment.getCardNumber(), new BigDecimal(1_000), payment.getAppointment()))
                 .thenReturn(payment.getPaymentId());
     }
 
     @Test
     public void testCreatePayment() {
-        Integer paymentId = paymentService.formPayment(1, "5450655032805450", new BigDecimal(1_000), new Double(1), "Thank you for dinner!");
+        Integer paymentId = paymentService.formPayment(1, "5450655032805450", new BigDecimal(1_000), "Thank you for dinner!");
         assertNotNull(paymentId);
         assertEquals(new Integer(1), paymentId);
     }
