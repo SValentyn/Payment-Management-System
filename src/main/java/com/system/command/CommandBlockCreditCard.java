632e4821559de2b8b12252623daca02377bcd543
Syniuk Valentyn <planet.sv01@gmail.com>
2020-01-30 03:40:40 +0200

    Added request attribute "cards" and "payments" in CommandBlockCreditCard.java

--- src/main/java/com/system/command/CommandBlockCreditCard.java
+++ src/main/java/com/system/command/CommandBlockCreditCard.java
@@ -4,6 +4,7 @@ import com.system.entity.User;
 import com.system.manager.ResourceManager;
 import com.system.service.AccountService;
 import com.system.service.CreditCardService;
+import com.system.service.PaymentService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,6 +24,9 @@ public class CommandBlockCreditCard implements ICommand {
         request.setAttribute("showAccounts", true);
         request.setAttribute("showAccountInfo", true);
         request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
+        int accountId = CreditCardService.getInstance().findCardByCardNumber(cardNumber).getAccountId();
+        request.setAttribute("cards", CreditCardService.getInstance().findCardsByAccountId(accountId));
+        request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(accountId));
 
         return ResourceManager.getInstance().getProperty(ResourceManager.HOME);
     }
