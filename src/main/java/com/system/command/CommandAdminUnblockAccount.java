723218857b059776b6cb0dcd2c54a0e571ed42ba
Syniuk Valentyn <planet.sv01@gmail.com>
2020-02-02 04:09:21 +0200

    Changed the behavior when an administrator unlocks a user account

--- src/main/java/com/system/command/CommandAdminUnblockAccount.java
+++ src/main/java/com/system/command/CommandAdminUnblockAccount.java
@@ -2,6 +2,8 @@ package com.system.command;
 
 import com.system.manager.ResourceManager;
 import com.system.service.AccountService;
+import com.system.service.CreditCardService;
+import com.system.service.PaymentService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,12 +15,14 @@ public class CommandAdminUnblockAccount implements ICommand {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
         String userId = (String) request.getSession().getAttribute("userId");
-
         String accountId = request.getParameter("accountId");
+
         if (accountId != null) {
             AccountService.getInstance().unblockAccount(Integer.parseInt(accountId));
-            request.setAttribute("showAccounts", true);
+
             request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(Integer.parseInt(userId)));
+            request.setAttribute("cards", CreditCardService.getInstance().findCardsByAccountId(Integer.valueOf(accountId)));
+            request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(Integer.valueOf(accountId)));
         }
 
         return ResourceManager.getInstance().getProperty(ResourceManager.ACCOUNTS_CONTROL);
