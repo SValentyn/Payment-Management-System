fbd78d791ad895e239e7248a588e9c1dc617efb9
Syniuk Valentyn <planet.sv01@gmail.com>
2020-02-26 20:59:58 +0200

    Changed the database initialization script (script.sql)

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,8 +1,12 @@
-SET session auto_increment_increment = 1;
-SET session character_set_client = utf8;
-SET session character_set_connection = utf8;
-SET session character_set_results = utf8;
-SET session character_set_server = utf8;
+SET NAMES utf8;
+SET CHARSET utf8;
+SET CHARACTER SET utf8;
+
+SET auto_increment_increment = 1;
+SET character_set_client = utf8;
+SET character_set_connection = utf8;
+SET character_set_results = utf8;
+SET character_set_server = utf8;
 
 SHOW VARIABLES LIKE 'auto_inc%';
 SHOW VARIABLES LIKE '%character%';
@@ -15,8 +19,6 @@ CREATE DATABASE heroku_a77bbc8929247c7
     COLLATE utf8_general_ci;
 
 USE heroku_a77bbc8929247c7;
-
-SET NAMES utf8;
 
 DROP TABLE IF EXISTS letters;
 
@@ -33,7 +35,7 @@ DROP TABLE IF EXISTS users;
 -- -- --
 CREATE TABLE users
 (
-    user_id  INT(11) AUTO_INCREMENT,
+    user_id  INT(11)      NOT NULL AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
     surname  VARCHAR(255) NOT NULL,
     phone    VARCHAR(255) NOT NULL,
@@ -45,44 +47,38 @@ CREATE TABLE users
   CHARACTER SET utf8
   DEFAULT COLLATE 'utf8_general_ci';
 
-# admin (p:111111)
-INSERT INTO users (name, surname, phone, email, password, role_id)
-VALUES ('Cristoforo', 'Colombo', '+393524594551', 'Cristoforo-Colombo@gmail.com', '96e79218965eb72c92a549dd5a330112',
-        2);
-
-# user (p:000000)
-INSERT INTO users (name, surname, phone, email, password, role_id)
-VALUES ('Fernando', 'de Magallanes', '+34645364524', 'Fernando-de-Magallanes@outlook.com',
-        '670b14728ad9902aecba32e22fa4f6bd', 1);
-
-# user (p:000001)
-INSERT INTO users (name, surname, phone, email, password, role_id)
-VALUES ('James', 'Cook', '+447465106475', 'James-Cook@gmail.com', '04fc711301f3c784d66955d98d399afb', 1);
-
-# user (p:000002)
-INSERT INTO users (name, surname, phone, email, password, role_id)
-VALUES ('Vasco', 'da Gama', '+351919131006', 'Vasco-da-Gama@gmail.com', '768c1c687efe184ae6dd2420710b8799', 1);
+# admin (pass: 111111)
+INSERT INTO users (user_id, name, surname, phone, email, password, role_id)
+VALUES (1, 'Cristoforo', 'Colombo', '+393524594551', 'Cristoforo-Colombo@gmail.com', '96e79218965eb72c92a549dd5a330112',
+        2),
+# user (pass: 000000)
+       (2, 'Fernando', 'de Magallanes', '+34645364524', 'Fernando-de-Magallanes@outlook.com',
+        '670b14728ad9902aecba32e22fa4f6bd', 1),
+# user (pass: 000001)
+       (3, 'James', 'Cook', '+447465106475', 'James-Cook@gmail.com', '04fc711301f3c784d66955d98d399afb', 1),
+# user (pass: 000002)
+       (4, 'Vasco', 'da Gama', '+351919131006', 'Vasco-da-Gama@gmail.com', '768c1c687efe184ae6dd2420710b8799', 1);
 -- -- --
 
 -- -- --
 CREATE TABLE roles
 (
-    id    INT(11) AUTO_INCREMENT,
+    id    INT(11)      NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 ) ENGINE = InnoDB
   CHARACTER SET utf8
   DEFAULT COLLATE 'utf8_general_ci';
 
-INSERT INTO roles (title)
-VALUES ('client'),
-       ('admin');
+INSERT INTO roles (id, title)
+VALUES (1, 'client'),
+       (2, 'admin');
 -- -- --
 
 -- -- --
 CREATE TABLE accounts
 (
-    account_id INT(11) AUTO_INCREMENT,
+    account_id INT(11)      NOT NULL AUTO_INCREMENT,
     user_id    INT(11)      NOT NULL,
     number     VARCHAR(255) NOT NULL,
     balance    DOUBLE       NOT NULL,
@@ -94,29 +90,19 @@ CREATE TABLE accounts
   CHARACTER SET utf8
   DEFAULT COLLATE 'utf8_general_ci';
 
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (2, '00000000000000000000', 9500.00, 'MXN', false);
-
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (2, '11111000000000000000', 7805.00, 'UAH', false);
-
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (2, '11111222220000000000', 3030.00, 'PLN', false);
-
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (3, '00000000000000000001', 10020.00, 'YER', true);
-
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (3, '11111000000000000001', 990.00, 'AUD', false);
-
-INSERT INTO accounts (user_id, number, balance, currency, is_blocked)
-VALUES (4, '00000000000000000002', 1115.00, 'MKD', false);
+INSERT INTO accounts (account_id, user_id, number, balance, currency, is_blocked)
+VALUES (1, 2, '00000000000000000000', 9500.00, 'MXN', false),
+       (2, 2, '11111000000000000000', 7805.00, 'UAH', false),
+       (3, 2, '11111222220000000000', 3030.00, 'PLN', false),
+       (4, 3, '00000000000000000001', 10020.00, 'YER', true),
+       (5, 3, '11111000000000000001', 990.00, 'AUD', false),
+       (6, 4, '00000000000000000002', 1115.00, 'MKD', false);
 -- -- --
 
 -- -- --
 CREATE TABLE credit_cards
 (
-    card_id    INT(11) AUTO_INCREMENT,
+    card_id    INT(11)      NOT NULL AUTO_INCREMENT,
     account_id INT(11)      NOT NULL,
     number     VARCHAR(255) NOT NULL,
     cvv        VARCHAR(255) NOT NULL,
@@ -128,23 +114,13 @@ CREATE TABLE credit_cards
   CHARACTER SET utf8
   DEFAULT COLLATE 'utf8_general_ci';
 
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (1, '0000000000000000', '200', '03/2021', true);
-
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (1, '0000007777000000', '717', '11/2021', true);
-
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (2, '0000008888000000', '809', '09/2020', false);
-
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (2, '0000009999000000', '191', '01/2022', true);
-
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (3, '0000009999000000', '500', '05/2021', true);
-
-INSERT INTO credit_cards (account_id, number, cvv, validity, is_active)
-VALUES (4, '4444000000000000', '404', '10/2020', true);
+INSERT INTO credit_cards (card_id, account_id, number, cvv, validity, is_active)
+VALUES (1, 1, '0000000000000000', '200', '03/2021', true),
+       (2, 1, '0000007777000000', '717', '11/2021', true),
+       (3, 2, '0000008888000000', '809', '09/2020', false),
+       (4, 2, '0000009999000000', '191', '01/2022', true),
+       (5, 3, '0000009999000000', '500', '05/2021', true),
+       (6, 4, '4444000000000000', '404', '10/2020', true);
 -- -- --
 
 -- -- --
@@ -163,11 +139,9 @@ CREATE TABLE payments
   CHARACTER SET utf8
   DEFAULT COLLATE 'utf8_general_ci';
 
-INSERT INTO payments (account_id, recipient_account_number, sum, appointment, date, `condition`)
-VALUES (2, '11111000000000000001', 2000.00, 'for accommodation', '01/02/2020', true);
-
-INSERT INTO payments (account_id, recipient_account_number, sum, appointment, date, `condition`)
-VALUES (4, '00000000000000000002', 400.00, 'charity', '01/10/2020', true);
+INSERT INTO payments (payment_id, account_id, recipient_account_number, sum, appointment, date, `condition`)
+VALUES (1, 2, '11111000000000000001', 2000.00, 'for accommodation', '01/02/2020', true),
+       (2, 4, '00000000000000000002', 400.00, 'charity', '01/10/2020', true);
 -- -- --
 
 -- -- --
