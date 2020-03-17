1fdfb413ccffdc2074b4136ff8f934f4c9fe9b6a
Syniuk Valentyn <planet.sv01@gmail.com>
2020-03-17 22:39:49 +0200

    Changed the behavior of the system when a bank card is blocked by the administrator

--- src/main/java/com/system/command/CommandAdminBlockCard.java
+++ src/main/java/com/system/command/CommandAdminBlockCard.java
@@ -1,38 +1,62 @@
 package com.system.command;
 
+import com.system.entity.Account;
 import com.system.manager.ResourceManager;
-import com.system.service.AccountService;
-import com.system.service.BankCardService;
-import com.system.service.LetterService;
-import com.system.service.PaymentService;
+import com.system.service.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandAdminBlockCard implements ICommand {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
-        request.getSession().setAttribute("numberOfLetters", LetterService.getInstance().findUnprocessedLetters().size());
-        request.setAttribute("blockCardError", false);
+        String page = ResourceManager.getInstance().getProperty(ResourceManager.ADMIN_SHOW_ACCOUNT_INFO);
 
-        String userId = (String) request.getSession().getAttribute("userId");
+        request.setAttribute("blockCardError", false);
+        request.getSession().setAttribute("numberOfLetters", LetterService.getInstance().findUnprocessedLetters().size());
+
+        // Data
         String cardNumber = request.getParameter("cardNumber");
         Integer accountId = BankCardService.getInstance().findCardByCardNumber(cardNumber).getAccountId();
 
-        if (cardNumber != null) {
-            BankCardService.getInstance().blockBankCard(cardNumber);
-
-            request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(Integer.valueOf(userId)));
-            request.setAttribute("cards", BankCardService.getInstance().findAllCardsByAccountId(accountId));
-            request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(accountId));
-        } else {
+        // Check
+        if (cardNumber == null) {
             request.setAttribute("blockCardError", true);
+            return page;
         }
 
-        return ResourceManager.getInstance().getProperty(ResourceManager.ADMIN_ACCOUNTS_CONTROL);
+        // Data
+        List<Account> accounts = AccountService.getInstance().findAllAccounts();
+        List<Integer> accountsIds = new ArrayList<>();
+        for (Account account : accounts) {
+            accountsIds.add(account.getAccountId());
+        }
+
+        // Check
+        if (!accountsIds.contains(accountId)) {
+            request.setAttribute("blockCardError", true);
+            return page;
+        }
+
+        // Action
+        BankCardService.getInstance().blockBankCard(cardNumber);
+
+        // Set Attributes
+        Account account = AccountService.getInstance().findAccountByAccountId(accountId);
+        request.setAttribute("account", account);
+        request.setAttribute("user", UserService.getInstance().findUserById(account.getUserId()));
+        request.setAttribute("paymentsEmpty", PaymentService.getInstance().findAllPaymentsByAccountId(accountId).isEmpty());
+        request.setAttribute("cardsEmpty", BankCardService.getInstance().findAllCardsByAccountId(accountId).isEmpty());
+        request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(accountId));
+        request.setAttribute("cards", BankCardService.getInstance().findAllCardsByAccountId(accountId));
+        request.setAttribute("showAccountError", false);
+
+        return page;
     }
 
 }
