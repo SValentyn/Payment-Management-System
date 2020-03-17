3e9cf7b96d1f261d13b87f5b037573b2f959f2e8
Syniuk Valentyn <planet.sv01@gmail.com>
2020-03-17 22:41:52 +0200

    The behavior of the system is changed when the user account is deleted by the administrator

--- src/main/java/com/system/command/CommandAdminDeleteAccount.java
+++ src/main/java/com/system/command/CommandAdminDeleteAccount.java
@@ -1,33 +1,62 @@
 package com.system.command;
 
+import com.system.entity.Account;
 import com.system.manager.ResourceManager;
-import com.system.service.AccountService;
-import com.system.service.LetterService;
+import com.system.service.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandAdminDeleteAccount implements ICommand {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
-        request.getSession().setAttribute("numberOfLetters", LetterService.getInstance().findUnprocessedLetters().size());
-        request.setAttribute("deleteAccountError", false);
+        String page = ResourceManager.getInstance().getProperty(ResourceManager.ADMIN_SHOW_ACCOUNT_INFO);
 
-        String userId = (String) request.getSession().getAttribute("userId");
+        request.setAttribute("deleteAccountError", false);
+        request.getSession().setAttribute("numberOfLetters", LetterService.getInstance().findUnprocessedLetters().size());
+
+        // Data
         String accountNumber = request.getParameter("accountNumber");
         Integer accountId = AccountService.getInstance().findAccountByAccountNumber(accountNumber).getAccountId();
 
-        if (userId != null & accountId != null) {
-            AccountService.getInstance().deleteAccountByAccountId(accountId);
-            request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(Integer.parseInt(userId)));
-        } else {
+        // Check
+        if (accountId == null) {
             request.setAttribute("deleteAccountError", true);
+            return page;
         }
 
-        return ResourceManager.getInstance().getProperty(ResourceManager.ADMIN_ACCOUNTS_CONTROL);
+        // Data
+        List<Account> accounts = AccountService.getInstance().findAllAccounts();
+        List<Integer> accountsIds = new ArrayList<>();
+        for (Account account : accounts) {
+            accountsIds.add(account.getAccountId());
+        }
+
+        // Check
+        if (!accountsIds.contains(accountId)) {
+            request.setAttribute("deleteAccountError", true);
+            return page;
+        }
+
+        // Action
+        AccountService.getInstance().deleteAccountByAccountId(accountId);
+
+        // Set Attributes
+        Account account = AccountService.getInstance().findAccountByAccountId(accountId);
+        request.setAttribute("account", account);
+        request.setAttribute("user", UserService.getInstance().findUserById(account.getUserId()));
+        request.setAttribute("paymentsEmpty", PaymentService.getInstance().findAllPaymentsByAccountId(accountId).isEmpty());
+        request.setAttribute("cardsEmpty", BankCardService.getInstance().findAllCardsByAccountId(accountId).isEmpty());
+        request.setAttribute("payments", PaymentService.getInstance().findAllPaymentsByAccountId(accountId));
+        request.setAttribute("cards", BankCardService.getInstance().findAllCardsByAccountId(accountId));
+        request.setAttribute("showAccountError", false);
+
+        return page;
     }
 
 }
