0389722f9c238291cf79990d7ae2b1faf34ab97c
Syniuk Valentyn <planet.sv01@gmail.com>
2020-03-31 04:49:45 +0300

    Changed validation rules in CommandAdminShowUserAccounts command (refactoring + added Validator's methods)

--- src/main/java/com/system/command/CommandAdminShowUserAccounts.java
+++ src/main/java/com/system/command/CommandAdminShowUserAccounts.java
@@ -1,16 +1,13 @@
 package com.system.command;
 
-import com.system.entity.User;
 import com.system.manager.ResourceManager;
 import com.system.service.AccountService;
 import com.system.service.LetterService;
-import com.system.service.UserService;
+import com.system.utils.Validator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CommandAdminShowUserAccounts implements ICommand {
 
@@ -23,33 +20,27 @@ public class CommandAdminShowUserAccounts implements ICommand {
         request.setAttribute("showUserAccountsError", false);
 
         // Data
-        User user = (User) request.getSession().getAttribute("viewableUser");
+        String userIdParam = request.getParameter("userId");
 
-        // Check
-        if (user == null) {
-            request.setAttribute("showUserAccountsError", true);
-            return page;
-        }
-
-        // Data
-        Integer userId = user.getUserId();
-        List<User> users = UserService.getInstance().findAllUsers();
-        List<Integer> usersIds = new ArrayList<>();
-        for (User aUser : users) {
-            usersIds.add(aUser.getUserId());
-        }
-
-        // Check
-        if (!usersIds.contains(userId)) {
+        // Validation
+        if (!validation(userIdParam)) {
             request.setAttribute("showUserAccountsError", true);
             return page;
         }
 
         // Set Attributes
-        request.setAttribute("accountsEmpty", AccountService.getInstance().findAllAccountsByUserId(userId).isEmpty());
-        request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(userId));
+        setRequestAttributes(request, Integer.parseInt(userIdParam));
 
         return page;
+    }
+
+    private boolean validation(String userId) throws SQLException {
+        return Validator.checkUserId(userId);
+    }
+
+    private void setRequestAttributes(HttpServletRequest request, Integer userId) throws SQLException {
+        request.setAttribute("accountsEmpty", AccountService.getInstance().findAllAccountsByUserId(userId).isEmpty());
+        request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(userId));
     }
 
 }
