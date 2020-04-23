1d8bea448ad63052c9586f4f9ae11d86b0763710
Syniuk Valentyn <planet.sv01@gmail.com>
2020-04-23 03:47:45 +0300

    Fixed a bug on the adminShowUser.jsp page (the country flag did not show by phone number if the admin profile was displayed)

--- src/main/webapp/resources/js/validator_adminShowUser.js
+++ src/main/webapp/resources/js/validator_adminShowUser.js
@@ -1,17 +1,37 @@
 // Elements on adminShowUser.jsp page to check
 let phone = document.querySelector("#phone");
+let phone_admin = document.querySelector("#phone_admin");
 
 /* Configuring the phone number input field.
  * "token" must be obtained on the api website */
-let iti = window.intlTelInput(phone, {
-    separateDialCode: true,
-    hiddenInput: "full_phone",
-    initialCountry: "auto",
-    geoIpLookup: function (callback) {
-        $.get('https://ipinfo.io', function () {
-        }, "jsonp").always(function (resp) {
-            let countryCode = (resp && resp.country) ? resp.country : "";
-            callback(countryCode);
-        });
-    },
-});
+let iti;
+
+if (phone != null) {
+    iti = window.intlTelInput(phone, {
+        separateDialCode: true,
+        hiddenInput: "full_phone",
+        initialCountry: "auto",
+        geoIpLookup: function (callback) {
+            $.get('https://ipinfo.io', function () {
+            }, "jsonp").always(function (resp) {
+                let countryCode = (resp && resp.country) ? resp.country : "";
+                callback(countryCode);
+            });
+        },
+    });
+}
+
+if (phone_admin != null) {
+    iti = window.intlTelInput(phone_admin, {
+        separateDialCode: true,
+        hiddenInput: "full_phone",
+        initialCountry: "auto",
+        geoIpLookup: function (callback) {
+            $.get('https://ipinfo.io', function () {
+            }, "jsonp").always(function (resp) {
+                let countryCode = (resp && resp.country) ? resp.country : "";
+                callback(countryCode);
+            });
+        },
+    });
+}
