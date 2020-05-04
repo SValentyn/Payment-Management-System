2937646eae6b17b9f891cbaad6d8838d471418a5
Syniuk Valentyn <planet.sv01@gmail.com>
2020-05-04 23:32:09 +0300

    Modified CommandUserDeleteAccount class due to new requirements (PRG pattern)

--- src/main/java/com/system/command/CommandUserDeleteAccount.java
+++ src/main/java/com/system/command/CommandUserDeleteAccount.java
@@ -1,16 +1,90 @@
 package com.system.command;
 
-import javax.servlet.ServletException;
+import com.system.entity.Account;
+import com.system.entity.User;
+import com.system.manager.HTTPMethod;
+import com.system.manager.ResourceManager;
+import com.system.manager.ServerResponse;
+import com.system.service.AccountService;
+import com.system.utils.Validator;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.sql.SQLException;
 
 public class CommandUserDeleteAccount implements ICommand {
 
+    // Default path
+    private String pathRedirect = ResourceManager.getInstance().getProperty(ResourceManager.USER_SHOW_ACCOUNT_SETTINGS);
+
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
-        return null;
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+
+        clearRequestAttributes(request);
+
+        String method = request.getMethod();
+        if (method.equalsIgnoreCase(HTTPMethod.GET.name()) || method.equalsIgnoreCase(HTTPMethod.POST.name())) {
+            pathRedirect = ResourceManager.getInstance().getProperty(ResourceManager.COMMAND_USER_SHOW_ACCOUNT_SETTINGS);
+
+            // Data
+            User user = (User) request.getSession().getAttribute("currentUser");
+            String accountIdParam = request.getParameter("accountId");
+
+            // Validation
+            if (!validation(request, user, accountIdParam)) {
+                return pathRedirect;
+            }
+
+            // Change redirect path
+            pathRedirect += "&accountId=" + accountIdParam;
+
+            // Action (delete account)
+            int status = AccountService.getInstance().deleteAccountByAccountId(Integer.valueOf(accountIdParam));
+            if (status == 0) {
+                setSessionAttributes(request, ServerResponse.ACCOUNT_DELETED_ERROR);
+            } else if (status == -1) {
+                setSessionAttributes(request, ServerResponse.ACCOUNT_HAS_FUNDS_ERROR);
+            } else {
+                pathRedirect = ResourceManager.getInstance().getProperty(ResourceManager.COMMAND_USER_SHOW_ACCOUNT_SETTINGS);
+                setSessionAttributes(request, ServerResponse.ACCOUNT_DELETED_SUCCESS);
+            }
+        }
+
+        return pathRedirect;
+    }
+
+    private boolean validation(HttpServletRequest request, User user, String accountIdParam) throws SQLException {
+
+        // Check
+        if (user == null) {
+            setSessionAttributes(request, ServerResponse.UNABLE_GET_USER);
+            return false;
+        }
+
+        // Validation accountId
+        if (!Validator.checkAccountId(accountIdParam)) {
+            setSessionAttributes(request, ServerResponse.UNABLE_GET_ACCOUNT_ID);
+            return false;
+        }
+
+        // Data
+        Account account = AccountService.getInstance().findAccountByAccountId(Integer.valueOf(accountIdParam));
+
+        // Checking that the account belongs to the user
+        if (!account.getUserId().equals(user.getUserId())) {
+            setSessionAttributes(request, ServerResponse.SHOW_ACCOUNT_ERROR);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void clearRequestAttributes(HttpServletRequest request) {
+        request.setAttribute("response", "");
+    }
+
+    private void setSessionAttributes(HttpServletRequest request, ServerResponse serverResponse) {
+        request.getSession().setAttribute("response", serverResponse.getResponse());
     }
 
 }
