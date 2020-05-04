03d3bdc18f97b4499f54394351cf5b964cdeb1f8
Syniuk Valentyn <planet.sv01@gmail.com>
2020-05-04 23:21:08 +0300

    Modified CommandUserUnblockAccount class due to new requirements (PRG pattern)

--- src/main/java/com/system/command/CommandUserUnblockAccount.java
+++ src/main/java/com/system/command/CommandUserUnblockAccount.java
@@ -1,9 +1,12 @@
 package com.system.command;
 
+import com.system.entity.Account;
 import com.system.entity.User;
+import com.system.manager.HTTPMethod;
 import com.system.manager.ResourceManager;
+import com.system.manager.ServerResponse;
 import com.system.service.AccountService;
-import com.system.service.UserService;
+import com.system.utils.Validator;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,20 +14,74 @@ import java.sql.SQLException;
 
 public class CommandUserUnblockAccount implements ICommand {
 
+    // Default path
+    private String pathRedirect = ResourceManager.getInstance().getProperty(ResourceManager.USER_SHOW_ACCOUNT_SETTINGS);
+
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws SQLException {
 
-        request.setAttribute("unblockAccountAlert", true);
-        request.setAttribute("unblockCardAlert", false);
+        clearRequestAttributes(request);
 
-        User user = (User) request.getSession().getAttribute("currentUser");
+        String method = request.getMethod();
+        if (method.equalsIgnoreCase(HTTPMethod.GET.name()) || method.equalsIgnoreCase(HTTPMethod.POST.name())) {
+            pathRedirect = ResourceManager.getInstance().getProperty(ResourceManager.COMMAND_USER_SHOW_ACCOUNT_SETTINGS);
 
-        // Set Attributes
-        request.getSession().setAttribute("currentUser", UserService.getInstance().findUserById(user.getUserId()));
-        request.setAttribute("showAccounts", true);
-        request.setAttribute("accounts", AccountService.getInstance().findAllAccountsByUserId(user.getUserId()));
+            // Data
+            User user = (User) request.getSession().getAttribute("currentUser");
+            String accountIdParam = request.getParameter("accountId");
 
-        return ResourceManager.getInstance().getProperty(ResourceManager.USER);
+            // Validation
+            if (!validation(request, user, accountIdParam)) {
+                return pathRedirect;
+            }
+
+            // Change redirect path
+            pathRedirect += "&accountId=" + accountIdParam;
+
+            // Action (block account)
+            int status = AccountService.getInstance().unblockAccount(Integer.valueOf(accountIdParam));
+            if (status == 0) {
+                setSessionAttributes(request, ServerResponse.ACCOUNT_UNBLOCKED_ERROR);
+            } else {
+                setSessionAttributes(request, ServerResponse.ACCOUNT_UNBLOCKED_SUCCESS);
+            }
+        }
+
+        return pathRedirect;
+    }
+
+    private boolean validation(HttpServletRequest request, User user, String accountIdParam) throws SQLException {
+
+        // Check
+        if (user == null) {
+            setSessionAttributes(request, ServerResponse.UNABLE_GET_USER);
+            return false;
+        }
+
+        // Validation accountId
+        if (!Validator.checkAccountId(accountIdParam)) {
+            setSessionAttributes(request, ServerResponse.UNABLE_GET_ACCOUNT_ID);
+            return false;
+        }
+
+        // Data
+        Account account = AccountService.getInstance().findAccountByAccountId(Integer.valueOf(accountIdParam));
+
+        // Checking that the account belongs to the user
+        if (!account.getUserId().equals(user.getUserId())) {
+            setSessionAttributes(request, ServerResponse.SHOW_ACCOUNT_ERROR);
+            return false;
+        }
+
+        return true;
+    }
+
+    private void clearRequestAttributes(HttpServletRequest request) {
+        request.setAttribute("response", "");
+    }
+
+    private void setSessionAttributes(HttpServletRequest request, ServerResponse serverResponse) {
+        request.getSession().setAttribute("response", serverResponse.getResponse());
     }
 
 }
